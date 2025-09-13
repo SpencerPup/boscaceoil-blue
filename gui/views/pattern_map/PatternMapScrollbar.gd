fe859dced3d069b13da3e8fd23f5871674b46a6e
Spencer <138608684+SpencerPup@users.noreply.github.com>
2025-09-13 11:22:51 -0400

    Made Arrangement Arrows Visible Without Hovering

--- gui/views/pattern_map/PatternMapScrollbar.gd
+++ gui/views/pattern_map/PatternMapScrollbar.gd
@@ -18,7 +18,7 @@ var can_scroll_right: bool = false
 var _button_holder: ButtonHolder = null
 var _tween: Tween = null
 var _left_visible: bool = false
-var _right_visible: bool = false
+var _right_visible: bool = true
 
 var _left_default_size: float = 0.0
 var _right_default_size: float = 0.0
@@ -39,7 +39,7 @@ func _ready() -> void:
 	_left_button.offset_left = -_left_default_size
 	_left_button.offset_right = 0
 	_right_button.offset_left = 0
-	_right_button.offset_right = _right_default_size
+	_right_button.offset_right = 0
 	
 	_left_button.mouse_exited.connect(_update_button_visibility)
 	_right_button.mouse_exited.connect(_update_button_visibility)
@@ -61,6 +61,9 @@ func set_button_offset(top_offset: float, bottom_offset: float) -> void:
 	_right_button.offset_top = top_offset
 	_right_button.offset_bottom = bottom_offset
 
+func _input(_event: InputEvent) -> void:
+	_update_button_visibility()
+
 
 func test_mouse_position() -> void:
 	_update_button_visibility()
@@ -72,31 +75,24 @@ func _update_button_visibility() -> void:
 	var left_area := Rect2(Vector2(0, _left_button.position.y), Vector2(_left_default_size, _left_button.size.y))
 	var right_area := Rect2(Vector2(size.x - _right_default_size, _right_button.position.y), Vector2(_right_default_size, _right_button.size.y))
 
-	if can_scroll_left && not _left_visible && left_area.has_point(mouse_position):
+	if can_scroll_left: # && not _left_visible && left_area.has_point(mouse_position):
 		_left_visible = true
-		_right_visible = false
-	elif can_scroll_right && not _right_visible && right_area.has_point(mouse_position):
-		_left_visible = false
-		_right_visible = true
-	elif (_left_visible || _right_visible) && (not left_area.has_point(mouse_position) && not right_area.has_point(mouse_position)):
-		_left_visible = false
-		_right_visible = false
 	else:
-		return # Nothing to do.
+		_left_visible = false
 	
-	if _tween:
-		_tween.kill()
-	_tween = get_tree().create_tween()
+	#if _tween:
+		#_tween.kill()
+	#_tween = get_tree().create_tween()
 	
 	if _left_visible:
-		_tween.tween_property(_left_button, "offset_left", 0, FADE_DURATION)
+		_left_button.offset_left = 0
 	else:
-		_tween.tween_property(_left_button, "offset_left", -_left_default_size, FADE_DURATION)
-	
-	if _right_visible:
-		_tween.tween_property(_right_button, "offset_right", 0, FADE_DURATION)
-	else:
-		_tween.tween_property(_right_button, "offset_right", _right_default_size, FADE_DURATION)
+		_left_button.offset_left = -_left_default_size
+	#
+	#if _right_visible:
+		#_tween.tween_property(_right_button, "offset_right", 0, FADE_DURATION)
+	#else:
+		#_tween.tween_property(_right_button, "offset_right", _right_default_size, FADE_DURATION)
 
 
 func _emit_hold_signal(hold_button: Button) -> void:
